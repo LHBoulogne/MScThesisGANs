d2c7098efadd5e512f8e369b04d23c31c5624fd7
LH Boulogne <lhboulogne@gmail.com>
2017-10-30 19:28:54 +0100

    updated show images

--- code/conditionalGAN/train_images/show_images.py
+++ code/conditionalGAN/train_images/show_images.py
@@ -3,25 +3,29 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-files = os.listdir('./')
-
-imgfiles = []
-for file in files:
-    if '.npy' in file:
-        imgfiles.append(file)
-
-imgfiles.sort(key=lambda f: int(filter(str.isdigit, f)))
+step = 10
 
 while True:
-    for file in imgfiles:
-        image = np.load(file)
-        plt.figure(1)
-        plt.gcf().clear()
+    files = os.listdir('./')
 
-        plt.imshow(image, cmap='gray')
-        plt.title('epoch: ' + filter(str.isdigit, file))
-        plt.draw()
-        plt.pause(0.00001)
+    imgfiles = []
+    for file in files:
+        if '.npy' in file:
+            imgfiles.append(file)
+
+    imgfiles.sort(key=lambda f: int(filter(str.isdigit, f)))
+
+    for idx, file in enumerate(imgfiles):
+        if idx % step == 0 or idx+1 == len(imgfiles):
+            image = np.load(file)
+            plt.figure(1)
+            plt.gcf().clear()
+
+            plt.imshow(image, cmap='gray')
+            plt.title('batch: ' + filter(str.isdigit, file))
+            plt.draw()
+            plt.pause(0.00001)
 
     plt.imshow(image, cmap='gray')
-    plt.show()
+    plt.draw()
+    plt.pause(5)
