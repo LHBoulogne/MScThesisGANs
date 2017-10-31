e6504ad9353b8f55ff67d146a19c0f4af17ad617
LH Boulogne <lhboulogne@gmail.com>
2017-10-31 09:31:22 +0100

    general show_images with cmd arguments

---
+++ code/conditionalGAN/show_images.py
@@ -0,0 +1,33 @@
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+step = int(sys.argv[1])
+
+wd = os.path.join('./', sys.argv[2])
+while True:
+    files = os.listdir(wd)
+
+    imgfiles = []
+    for file in files:
+        if '.npy' in file:
+            imgfiles.append(file)
+
+    imgfiles.sort(key=lambda f: int(filter(str.isdigit, f)))
+
+    for idx, file in enumerate(imgfiles):
+        if idx % step == 0 or idx+1 == len(imgfiles):
+            image = np.load(os.path.join(wd, file))
+            plt.figure(1)
+            plt.gcf().clear()
+
+            plt.imshow(image, cmap='gray')
+            plt.title('batch: ' + filter(str.isdigit, file))
+            plt.draw()
+            plt.pause(0.00001)
+
+    plt.imshow(image, cmap='gray')
+    plt.draw()
+    plt.pause(5)
