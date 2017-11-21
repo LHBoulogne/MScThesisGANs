87a6b33e3004fcbb92063c08b05d3f7329a5750c
LH Boulogne <lhboulogne@gmail.com>
2017-11-21 10:21:36 +0100

    added show images here

---
+++ code/dcgan2-pytorch/show_images.py
@@ -0,0 +1,33 @@
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+step = int(sys.argv[1])
+
+wd = os.path.join('./', sys.argv[2])
+while True:
+    files = os.listdir(wd)
+
+    imgfiles = []
+    for file in files:
+        if '.npy' in file:
+            imgfiles.append(file)
+
+    imgfiles.sort(key=lambda f: int(''.join(list(filter(str.isdigit, f)))))
+
+    for idx, file in enumerate(imgfiles):
+        if idx % step == 0 or idx+1 == len(imgfiles):
+            image = np.load(os.path.join(wd, file))
+            plt.figure(1)
+            plt.gcf().clear()
+
+            plt.imshow(image, cmap='gray', interpolation='nearest')
+            plt.title('batch: ' + ''.join(list(filter(str.isdigit, file))))
+            plt.draw()
+            plt.pause(0.00001)
+
+    plt.imshow(image, cmap='gray', interpolation='nearest')
+    plt.draw()
+    plt.pause(5)
