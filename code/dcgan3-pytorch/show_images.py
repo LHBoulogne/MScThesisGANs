e151348142d7a01308fa0676f2d882bb5d94045a
L.H.Boulogne@student.rug.nl <s2366681@peregrine.hpc.rug.nl>
2017-11-23 12:26:01 +0100

    copied show-imgs to next folder

---
+++ code/dcgan3-pytorch/show_images.py
@@ -0,0 +1,34 @@
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+step = int(sys.argv[1])
+
+wd = os.path.join('./', sys.argv[2])
+while True:
+    files = os.listdir(wd)
+
+    imgfiles = []
+    for file in files:
+        if '.npy' in file:
+            imgfiles.append(file)
+
+    imgfiles.sort(key=lambda f: int(''.join(list(filter(str.isdigit, f)))))
+
+    for idx, file in enumerate(imgfiles):
+        if idx % step == 0 or idx+1 == len(imgfiles):
+            image = np.load(os.path.join(wd, file))
+            image = (image+1)/2
+            plt.figure(1)
+            plt.gcf().clear()
+            
+            plt.imshow(image)
+            plt.title('batch: ' + ''.join(list(filter(str.isdigit, file))))
+            plt.draw()
+            plt.pause(0.0001)
+
+    plt.imshow(image)
+    plt.draw()
+    plt.pause(5)
