1ee5ee7daebf68b461f3a4a64a93a5b431d0ce97
LH Boulogne <lhboulogne@gmail.com>
2018-01-30 15:20:34 +0100

    set config in staged_main.py

--- src/staged_main.py
+++ src/staged_main.py
@@ -3,11 +3,15 @@ from gan.gan import *
 
 if __name__ == "__main__":
     config = parse_args()
+    config.labels2 = [1,2,3,4,5,6,7,8,9,0]
+    config.batches = 2500
+    config.epochs = 1
     gan = GAN(config)
     if config.train:
         gan.train()
         gan.save()
 
+        config.batches = 25000
         config.loadfolder = config.savefolder
         config.savefolder = config.savefolder+'_nofives'
         config.labels2 = [1,2,3,4,  6,7,8,9,0]
