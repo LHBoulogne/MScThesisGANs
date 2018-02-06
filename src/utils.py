a1f3eed55ef6b1cb559b22366ea31fb7781ed2cd
LH Boulogne <lhboulogne@gmail.com>
2018-02-06 11:39:44 +0100

    isinstance fix

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,9 @@ import torch
 
 def cuda(xs):
     if torch.cuda.is_available():
-        if xs.isinstance(list):
+        if isinstance(xs,list):
             return [cuda(x) for x in xs]
-        if xs.isinstance(tuple):
+        if isinstance(xs,tuple):
             return tuple([cuda(x) for x in xs])
         return xs.cuda()
     return xs
