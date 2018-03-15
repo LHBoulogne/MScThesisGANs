ff5ab82cefa0b9118c3a88c648ac372d077bf97a
LH Boulogne <lhboulogne@gmail.com>
2018-03-15 15:11:45 +0100

    debug

--- src/gan/model/generator/dcgan.py
+++ src/gan/model/generator/dcgan.py
@@ -77,9 +77,7 @@ class Generator(nn.Module):
             features_b = self.main(inp_b)
             out_b = self.last_b(features_b)
 
-
-        self.train()            
-
+            self.train()            
 
             import matplotlib.pyplot as plt
             import numpy as np
