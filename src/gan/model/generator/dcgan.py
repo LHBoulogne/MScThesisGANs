a71a825feb18cd48a4aad827274e27520508b409
LH Boulogne <lhboulogne@gmail.com>
2018-03-15 15:03:51 +0100

    debug

--- src/gan/model/generator/dcgan.py
+++ src/gan/model/generator/dcgan.py
@@ -53,6 +53,13 @@ class Generator(nn.Module):
         return last
 
     def forward(self, z, c_a=None, c_b=None): #for accogans and cogans
+
+        c_a[0][0] = 0#TODO REMOVE
+        c_a[0][1] = 1#TODO REMOVE
+
+        c_b[0][0] = 0#TODO REMOVE
+        c_b[0][1] = 1#TODO REMOVE
+
         if self.auxclas:
             inp_a = torch.cat((z, c_a), 1)
             if self.coupled:
