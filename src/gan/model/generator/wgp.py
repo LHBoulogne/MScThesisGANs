562d89aef792dd5104bed8e637e92e513f2225de
LH Boulogne <lhboulogne@gmail.com>
2018-03-22 12:04:01 +0100

    changed variable name

--- src/gan/model/generator/wgp.py
+++ src/gan/model/generator/wgp.py
@@ -32,15 +32,15 @@ class Generator(nn.Module):
 
         self.main = nn.Sequential(*layers)
 
-        self.last_a = self.get_last_layers()
+        self.last_a = self.get_last_layers(config.g_dim)
         if config.coupled:
-            self.last_b = self.get_last_layers()
+            self.last_b = self.get_last_layers(config.g_dim)
 
         weight_init(self, config.weight_init)
 
-    def get_last_layers(self):
+    def get_last_layers(self, in_dim):
         last = nn.Sequential(
-            nn.ConvTranspose2d(config.g_dim, 3, 5, 2, padding=2, output_padding=1),
+            nn.ConvTranspose2d(in_dim, 3, 5, 2, padding=2, output_padding=1),
             nn.Tanh()
         )
         return last
