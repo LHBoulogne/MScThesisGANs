71f8ddc8549fe6e3e8a3d3b5435397c19ba2fd76
LH Boulogne <lhboulogne@gmail.com>
2018-03-27 10:39:44 +0200

    undid grad penalty change

--- src/gan/trainer.py
+++ src/gan/trainer.py
@@ -125,8 +125,8 @@ class GANTrainer():
             x = inp_real[idx].data
             x_wave = inp_fake[idx].data
 
-            #x_hat = e*x + (1-e)*x_wave
-            x_hat = e*x_wave + (1-e)*x # SHOULD BE UNDONE!
+            x_hat = e*x + (1-e)*x_wave
+            #x_hat = e*x_wave + (1-e)*x
 
             inp_hat += (utils.cuda(Variable(x_hat, requires_grad=True)),)
 
