7f84f6c699f442846e211bbab721c476172b63a8
LH Boulogne <lhboulogne@gmail.com>
2018-03-29 17:49:26 +0200

    cuda onhot

--- src/gan/aux/aux.py
+++ src/gan/aux/aux.py
@@ -1,4 +1,5 @@
 import torch
+import utils
 
 def rescale(t):
     t.div_(0.5).add_(-1)
@@ -13,5 +14,6 @@ def to_one_hot(categories, y):
     y = y.view(-1,1)
     onehot = torch.FloatTensor(batch_size, categories)
     torch.zeros(batch_size, categories, out=onehot)
+    onehot = utils.cuda(onehot)
     onehot.scatter_(1, y, 1)
     return onehot
