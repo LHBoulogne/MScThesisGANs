19b9c3092a0ee676c00ebee7dbeedab761884068
LH Boulogne <lhboulogne@gmail.com>
2018-05-07 11:10:00 +0200

    reimplemented old wgan

--- src/gan/trainer.py
+++ src/gan/trainer.py
@@ -115,6 +115,55 @@ class GANTrainer():
         return -d_out[0].mean()
 
 
+    def grad_penalty_old(self, inp_real, inp_fake, D):
+        inp_hat = ()
+        for idx in range(len(inp_fake)):
+            e = utils.cuda(torch.rand(self.config.mini_batch_size, 1,1,1))
+
+            x = inp_real[idx].data
+            x_wave = inp_fake[idx].data
+
+            x_hat = e*x + (1-e)*x_wave
+            #x_hat = e*x_wave + (1-e)*x
+
+            inp_hat += (utils.cuda(Variable(x_hat, requires_grad=True)),)
+
+        out_hat = D(*inp_hat)
+
+        gps = ()
+        for idx in range(len(out_hat)):
+            gradient = grad(out_hat[idx][0], inp_hat[idx],
+                grad_outputs = utils.cuda(torch.ones(out_hat[idx][0].size())), 
+                create_graph = True)[0]
+            gradient = gradient.view(self.config.mini_batch_size, -1)
+            gp = ((gradient.norm(p=2, dim=1) - 1)**2).mean()
+            gps += (gp,)
+            
+        return gps
+
+
+    def compute_D_error_WGAN_gp_old(self, inp_real, inp_fake, out_real, out_fake, D):
+        gps = self.grad_penalty_old(inp_real, inp_fake, D)
+        separate_errors = ()
+        error = 0
+        for idx in range(len(out_fake)):
+            err = out_fake[idx][0].mean() - out_real[idx][0].mean() + self.config.gp_coef*gps[idx]
+            separate_errors += (err,)
+            error += err
+
+        return error, separate_errors
+
+    def compute_G_error_WGAN_gp_old(self, d_out):
+        separate_errors = ()
+        error = 0
+        for idx in range(len(d_out)):
+            err = -d_out[idx][0].mean()
+            separate_errors += (err,)
+            error += err
+
+        return error, separate_errors
+
+
     def update_discriminator(self, G, D):
         if self.config.mini_batch_size != self.this_batch_size:
             return False
@@ -143,37 +192,41 @@ class GANTrainer():
                 d_inp_hat_list += (utils.cuda(Variable(x_hat, requires_grad=True)),)
             d_out_hat_list = D(*d_inp_hat_list)
 
+        if algorithm == 'wgan_gp_old':
+            src_error, _ = self.compute_D_error_WGAN_gp_old(d_inp_real_list, d_inp_fake_list, d_out_real_list, d_out_fake_list, D)
+            src_error.backward()
+            self.d_opt.step()
+        else:
+            for idx in range(len(d_out_real_list)):
+                d_out_real = d_out_real_list[idx]
+                d_inp_real = d_inp_real_list[idx]
+                d_out_fake = d_out_fake_list[idx]
+                d_inp_fake = d_inp_fake_list[idx]
 
-        for idx in range(len(d_out_real_list)):
-            d_out_real = d_out_real_list[idx]
-            d_inp_real = d_inp_real_list[idx]
-            d_out_fake = d_out_fake_list[idx]
-            d_inp_fake = d_inp_fake_list[idx]
-
-            if self.config.algorithm == 'default':
-                src_error_real = self.src_error(d_out_real[0], self.y_real)
-                src_error_fake = self.src_error(d_out_fake[0], self.y_fake)
-                src_error = (src_error_real, src_error_fake)
-            elif self.config.algorithm == 'wgan_gp':
-                error, w_dist = self.WGAN_D_src_error(d_inp_real, d_inp_fake, d_inp_hat_list[idx], d_out_real[0], d_out_fake[0], d_out_hat_list[idx][0])
-                src_error = (error, w_dist)
-            
-            error = sum(src_error)
-            if self.config.auxclas:
-                if self.config.c_algorithm == 'default':
-                    class_error_real = self.class_error(d_out_real[1], self.c_reals[idx])
-                    class_error_fake = self.class_error(d_out_fake[1], self.c_fakes[idx])
-                    class_error = (class_error_real, class_error_fake)
-                elif self.config.c_algorithm == 'wgan_gp':
-                    raise RuntimeError("c_algorithm wgan_gp: Not implemented")
-                error += sum(class_error)
+                if self.config.algorithm == 'default':
+                    src_error_real = self.src_error(d_out_real[0], self.y_real)
+                    src_error_fake = self.src_error(d_out_fake[0], self.y_fake)
+                    src_error = (src_error_real, src_error_fake)
+                elif self.config.algorithm == 'wgan_gp':
+                    error, w_dist = self.WGAN_D_src_error(d_inp_real, d_inp_fake, d_inp_hat_list[idx], d_out_real[0], d_out_fake[0], d_out_hat_list[idx][0])
+                    src_error = (error, w_dist)
                 
-                self.error_storage.store_errors('D', idx, src_error, class_error)
-            else:
-                self.error_storage.store_errors('D', idx, src_error)
-            
-            error.backward()
-        self.d_opt.step()
+                error = sum(src_error)
+                if self.config.auxclas:
+                    if self.config.c_algorithm == 'default':
+                        class_error_real = self.class_error(d_out_real[1], self.c_reals[idx])
+                        class_error_fake = self.class_error(d_out_fake[1], self.c_fakes[idx])
+                        class_error = (class_error_real, class_error_fake)
+                    elif self.config.c_algorithm == 'wgan_gp':
+                        raise RuntimeError("c_algorithm wgan_gp: Not implemented")
+                    error += sum(class_error)
+                    
+                    self.error_storage.store_errors('D', idx, src_error, class_error)
+                else:
+                    self.error_storage.store_errors('D', idx, src_error)
+                
+                error.backward()
+            self.d_opt.step()
 
         return True
 
@@ -189,27 +242,32 @@ class GANTrainer():
         d_out_list = D(*g_out)
 
         # perform backward pass and update
-        for idx in range(len(d_out_list)):
-            d_out = d_out_list[idx]
-            
-            if self.config.algorithm == 'default':
-                src_error = self.src_error(d_out[0], self.y_real)
-            elif self.config.algorithm == 'wgan_gp':
-                src_error = self.WGAN_G_src_error(d_out[0])
-            
-            error = src_error
-            if self.config.auxclas:
-                if self.config.c_algorithm == 'default':
-                    class_error = self.class_error(d_out[1], self.c_reals[idx])
-                elif self.config.c_algorithm == 'wgan_gp':
-                    raise RuntimeError("c_algorithm wgan_gp: Not implemented")
-                error += class_error
+        if self.config.algorithm == 'wgan_gp_old':
+            error, separate_errors = self.compute_G_error_WGAN_gp_old(d_out_list)
+            sum(error).backward()
+            self.g_opt.step()
+        else :
+            for idx in range(len(d_out_list)):
+                d_out = d_out_list[idx]
                 
-                self.error_storage.store_errors('G', idx, src_error, class_error)
-            else:
-                self.error_storage.store_errors('G', idx, src_error)
-            error.backward()
-        
-        self.g_opt.step()
+                if self.config.algorithm == 'default':
+                    src_error = self.src_error(d_out[0], self.y_real)
+                elif self.config.algorithm == 'wgan_gp':
+                    src_error = self.WGAN_G_src_error(d_out[0])
+                
+                error = src_error
+                if self.config.auxclas:
+                    if self.config.c_algorithm == 'default':
+                        class_error = self.class_error(d_out[1], self.c_reals[idx])
+                    elif self.config.c_algorithm == 'wgan_gp':
+                        raise RuntimeError("c_algorithm wgan_gp: Not implemented")
+                    error += class_error
+                    
+                    self.error_storage.store_errors('G', idx, src_error, class_error)
+                else:
+                    self.error_storage.store_errors('G', idx, src_error)
+                error.backward()
+            
+            self.g_opt.step()
         
         return True
