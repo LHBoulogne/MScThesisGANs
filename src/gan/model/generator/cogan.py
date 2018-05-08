8a7a92195d5dbbced4ab962af0044daa57aa1069
LH Boulogne <lhboulogne@gmail.com>
2018-05-08 12:40:18 +0200

    coupled

--- src/gan/model/generator/cogan.py
+++ src/gan/model/generator/cogan.py
@@ -13,6 +13,7 @@ class Generator(nn.Module):
     def __init__(self, config):
         super(Generator, self).__init__()
         self.auxclas = config.auxclas
+        self.coupled = config.coupled
         c_len = 0
         if config.auxclas:
             c_len = sum(config.categories)
@@ -30,7 +31,8 @@ class Generator(nn.Module):
         self.bn3 = nn.BatchNorm2d(config.g_dim, affine=False)
         self.prelu3 = nn.PReLU()
         self.dconv4_a = nn.ConvTranspose2d(config.g_dim, 1, kernel_size=6, stride=1, padding=1)
-        self.dconv4_b = nn.ConvTranspose2d(config.g_dim, 1, kernel_size=6, stride=1, padding=1)
+        if self.coupled:
+            self.dconv4_b = nn.ConvTranspose2d(config.g_dim, 1, kernel_size=6, stride=1, padding=1)
         self.sig4 = nn.Tanh()
         
         weight_init(self, config.weight_init)
@@ -52,7 +54,9 @@ class Generator(nn.Module):
         
     def forward(self, z, c_a=None, c_b=None):
         h3_a = self.singleForward(z, c_a)
-        h3_b = self.singleForward(z, c_b)
         out_a = self.sig4(self.dconv4_a(h3_a))
-        out_b = self.sig4(self.dconv4_b(h3_b))
-        return out_a, out_b
+        if self.coupled:
+            h3_b = self.singleForward(z, c_b)
+            out_b = self.sig4(self.dconv4_b(h3_b))
+            return out_a, out_b
+        return (out_a,)
