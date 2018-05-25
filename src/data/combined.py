33810a813bbe0ba276dcde3e2d1f92341f54e011
LH Boulogne <lhboulogne@gmail.com>
2018-05-25 14:18:07 +0200

    fixed sampling

--- src/data/combined.py
+++ src/data/combined.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 import numpy as np
+import torch
 
 #Samples randomly from dataset1 and dataset2.
 class CombinedDataset(Dataset) :
@@ -13,11 +14,19 @@ class CombinedDataset(Dataset) :
 
     
     def get_random_labelbatch(self, batchsize):
-        n = np.random.randint(self.length)
-        if n < len(self.dataset1):
-            return self.dataset1.get_random_labelbatch(batchsize)
-        return self.dataset2.get_random_labelbatch(batchsize)
-
+        d1count = 0
+        d2count = 0
+        for it in range(batchsize):
+            n = np.random.randint(self.length)
+            if n < len(self.dataset1):
+                d1count += 1
+            else:
+                d2count += 1
+        batch1 = self.dataset1.get_random_labelbatch(d1count)
+        batch2 = self.dataset2.get_random_labelbatch(d2count)
+        return torch.cat([batch1, batch2], 0)
+        
+        
     def __getitem__(self, idx):
         if idx < len(self.dataset1):
             return self.dataset1[idx]
